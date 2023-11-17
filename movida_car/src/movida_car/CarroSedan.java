20d61f8647a1fbc117ee134a0f338a07e0516621
roberttmag <145805081+roberttmag@users.noreply.github.com>
2023-11-16 21:38:35 -0300

    CarroSedan.java

--- movida_car/src/movida_car/CarroSedan.java
+++ movida_car/src/movida_car/CarroSedan.java
@@ -1,19 +1,13 @@
 package movida_car;
 
 public class CarroSedan extends Veiculo {
-
-    // Construtor que chama o construtor da classe pai (Veiculo)
     public CarroSedan(String marca, String modelo, int ano, double diaria) {
         super(marca, modelo, ano, diaria);
     }
-
-    // Sobrescrever o método setDiaria para aplicar 10% a mais sobre o preço informado
     @Override
     public void setDiaria(double valor) {
         super.setDiaria(valor * 1.1); 
     }
-
-    // Sobrescrever toString para incluir informações específicas do CarroSedan
     @Override
     public String toString() {
     	return String.format("Carro Sedan - Marca = %s, Modelo = %s, Ano = %d, Diaria = %.2f R$",
